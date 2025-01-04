b29a5588016c18f87a174504f887a641ae8e3081
Ghazi Meddeb <ghazimdb@gmail.com>
2025-01-04 22:57:19 +0100

    Update TP1.cpp

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -4,21 +4,21 @@
 
 using namespace std;
 
-class fichier {
+class Fichier {
     char* name;
     char* mode;
     FILE* file;
     int size_len;
 
 public:
-    fichier(){
+    Fichier(){
         name = nullptr;
         mode = nullptr;
         file = nullptr;
         size_len = 0;
     }
 
-    fichier(char* _name, char* _mode, int _size_len) {
+    Fichier(char* _name, char* _mode, int _size_len) {
         name = new char[strlen(_name) + 1];
         mode = new char[strlen(_mode) + 1];
 
@@ -34,7 +34,7 @@ public:
         }
     }
 
-    ~fichier(){
+    ~Fichier(){
         delete[] name;
         delete[] mode;
         fclose(file);
@@ -135,12 +135,75 @@ public:
         }
     }
 
-    
+    void createInFile() {
+        char* str = new char[size_len + 1];
+        while (gets(str) && *str) {
+            writeFile(str);
+        }
+    }
+
+    void viewFile() {
+        if (file) {
+            fclose(file);
+        }
+
+        file = fopen(name, "r");
+        if (!file) {
+            perror("File is not open");
+        }
+        char buffer[size_len];
+        while (fgets(buffer, size_len, file) != nullptr) {
+            cout << buffer << endl;
+        }
+        fclose(file);
+
+        file = fopen(name, mode);
+        if (!file) {
+            perror("File is not open");
+        }
+    }
+
+    Fichier operator+ (Fichier& f1) {
+        int max = (this->size_len > f1.size_len) ? this->size_len : f1.size_len;
+        char *new_name= new char[strlen(this->name)+strlen(f1.name)+1];
+        cin>>new_name;
+        Fichier f0(new_name, "a+", max);
+        char buffer[max];
+        fclose(file);
+        file = fopen(name, "r");
+        if (!file) {
+            perror("File is not open");
+        }
+        f1.file= fopen(f1.name,"r");
+        if (!f1.file) {
+            perror("File is not open");
+        }
+        //copying the content of the first file to the new file
+        while (fgets(buffer, max, file) != nullptr) {
+            f0.writeFile(buffer);
+        }
+        //copying the content of the second file to the new file
+        while (fgets(buffer, max, f1.file) != nullptr) {
+            f0.writeFile(buffer);
+        }
+        //returning first and second file to their original state
+        fclose(file);
+        fclose(f1.file);
+        file= fopen(name, mode);
+        f1.file= fopen(f1.name, f1.mode);
+        //setting the mode of the new file to write
+        f0.setMode("w");
+        return f0;
+    }
 };
 
-int main() {
-    fichier f("Taki", "Here", 6);
-    cout << "Hello World!" << endl;
-    cout << strlen(f.getName()) << endl;  // Use the getter
-    return 0;
+int Main()
+{
+    Fichier ville1 ("Ville.txt","w",15);
+    Fichier ville2("new.txt","w",25);
+    Fichier f3;
+    ville1.createInFile();
+    ville2.createInFile();
+    f3=ville1+ville2;
+    f3.viewFile();
 }
